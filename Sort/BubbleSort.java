d4e39dcd920d0bbef3cc52bb199d887430be75c9
PARKYOONJUNG <201602001@o.cnu.ac.kr>
2019-06-02 16:01:07 +0900

    Bubble Sort
    Bubble Sort

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,58 @@
+package HW04;
+//201602001 박윤정
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numlist = new ArrayList<Integer>(); 
+		try { // 파일 읽어와서 배열에 넣기
+			FileReader fr = new FileReader("C://Users//user//Desktop//data04.txt"); // 파일을 읽어올 경로 설정
+			String line = "";
+			int i = 0;
+			BufferedReader br = new BufferedReader(fr); // 버퍼 생성자 객체 br
+			while ((line = br.readLine()) != null) { // 파일 내용이 비어있을 때까지
+				StringTokenizer st = new StringTokenizer(line, ","); // " "을 기준으로 읽어온 line을 나눠준다
+				while (st.hasMoreTokens()) { // token이 더 이상 없을 까지
+					numlist.add(Integer.parseInt(st.nextToken())); // 리스트에 추가
+					i++;
+				}
+			}
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // 예외상황 처리
+		BubbleSort(numlist);	
+		try { // 배열에 있는 내용을 파일로 생성
+			int i = 0;
+			FileWriter fw = new FileWriter("C://Users//user//Desktop//data04_Sort_Bub.txt"); // 파일을 저장할 경로 설정
+			BufferedWriter bw = new BufferedWriter(fw); 
+			while(i<numlist.size()) {
+				bw.write(Integer.toString(numlist.get(i++)));	
+				if(i != numlist.size()) bw.write(","); // 마지막 원소 뒤에는 쉼표 안붙게 처리
+			}
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // 예외상황 처리
+	}
+
+	private static void BubbleSort(List<Integer> numlist) {
+			for(int i = numlist.size()-1;i>=0;i--) { 
+				for(int j=0;j<i;j++) {
+					if(numlist.get(j) > numlist.get(j+1)) {
+						int temp = numlist.get(j); // swap
+						numlist.set(j,numlist.get(j+1));
+						numlist.set(j+1,temp);
+					}
+				}
+			}
+		}
+}
